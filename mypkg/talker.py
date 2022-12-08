becd04b53b98b3456ec1be1a0e7a9aa01528b8fd
Kome-cyber <s21c1003ak@s.chibakoudai.jp>
2022-12-08 12:53:41 +0900

    Initial Commit

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,28 +1,18 @@
 import rclpy
-from rclpy.node import Node
+from rclpy.node import Node 
 from std_msgs.msg import Int16
 
-class Talker():
-    def _init_(self, node):
-        self.pub = node.create_publisher(Int16, "countup", 10)
-        self.n = 0
-        node.create_timer(0.5, self.cb)
-
-def cb(self):
-    msg = Int16()
-    msg.data = self.n
-    self.pub.publish(msg)
-    self.n += 1
-
 rclpy.init()
-node = Node("talker")
-talker = Talker()
-rclpy.spin(node)
+node = Node("talker")   
+pub = node.create_publisher(Int16, "countup", 10) 
+n = 0
 
-def main():
-    rclpy.init()
-    node = Node("talker")
-    talker = Talker(node)
-    rclpy.spin(node)
-    if __name__ == '__main__':
-        main()
+def cb():
+    global n  
+        msg = Int16() 
+            msg.data = n
+                pub.publish(msg)    
+                        n += 1
+                            
+                            node.create_timer(0.5, cb)
+                            rclpy.spin(node)            
