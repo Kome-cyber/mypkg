3264c1e2325f5bbdbf8b4605afc175387faa7d07
Kome-cyber <s21c1003ak@s.chibakoudai.jp>
2022-12-27 13:12:58 +0900

    Initial Commit

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -9,10 +9,10 @@ n = 0
 
 def cb():
     global n  
-        msg = Int16() 
-            msg.data = n
-                pub.publish(msg)    
-                        n += 1
+    msg = Int16() 
+    msg.data = n
+    pub.publish(msg)    
+    n += 1
                             
-                            node.create_timer(0.5, cb)
-                            rclpy.spin(node)         
+node.create_timer(0.5, cb)
+rclpy.spin(node)         
